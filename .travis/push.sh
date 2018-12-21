c0fb6c763f6e03e033dd3d202f644e1eaef2a6df
CrazyMax <crazy-max@users.noreply.github.com>
2018-12-21 21:55:19 +0100

    Not push if not tag

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,6 +7,11 @@ QUAY_USERNAME=${QUAY_USERNAME:-crazymax}
 QUAY_REPONAME=${QUAY_REPONAME:-xray}
 MICROBADGER_HOOK=${MICROBADGER_HOOK:-https://hooks.microbadger.com/images/crazymax/xray/Pj8_AtfcJIbA8HOMKnyabZFJRy4=}
 
+if [[ -z $(docker image ls ${DOCKER_USERNAME}/${DOCKER_REPONAME} --format "{{.ID}}") ]]; then
+  echo "No tag to push"
+  exit 0
+fi
+
 if [ ! -z ${DOCKER_PASSWORD} ]; then
   echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
   docker push ${DOCKER_USERNAME}/${DOCKER_REPONAME}
