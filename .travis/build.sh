01a604420700fc519422af83f15c6118767f0bc2
CrazyMax <crazy-max@users.noreply.github.com>
2018-12-21 21:45:06 +0100

    Update build script

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 DOCKER_USERNAME=${DOCKER_USERNAME:-crazymax}
@@ -11,18 +10,34 @@ if [ -z ${TOKEN} -o ${TOKEN} == "null" ]; then
   exit 1
 fi
 
-function docker_pull() {
-  echo "### Pulling Xray $2 $3 from $1..."
-  TAG_EXISTS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${DOCKER_USERNAME}/${DOCKER_REPONAME}/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$2-$3\"] | any")
-  echo "Check tag exists : $TAG_EXISTS"
-  if [ "${TAG_EXISTS}" = "true" ]; then
-    echo "Tag $2-$3 already exists... Skipping..."
-  else
-    docker pull $1:$3
-    docker tag $1:$3 ${DOCKER_USERNAME}/${DOCKER_REPONAME}:$2-$3
-    docker tag $1:$3 quay.io/${DOCKER_USERNAME}/${DOCKER_REPONAME}:$2-$3
+TAG_LIST=""
+function get_docker_tags() {
+  for TAG in $(curl -s -H "Authorization: JWT ${TOKEN}" $1 | jq -rc '.results[]'); do
+    TAG_LIST="$(echo "$TAG" | jq -r .name),$TAG_LIST"
+  done
+  NEXT_PAGE=$(curl -s -H "Authorization: JWT ${TOKEN}" $1 | jq -r '.next')
+  if [[ ${NEXT_PAGE} != "null" ]]; then
+    get_docker_tags ${NEXT_PAGE}
   fi
 }
+
+function docker_pull() {
+  echo "### Pulling Xray $2 $3 from $1..."
+  echo "Check if tag exists..."
+  for TAG in $(echo ${TAG_LIST} | sed "s/,/ /g"); do
+    if [[ "$TAG" = "$2-$3" ]]; then
+      echo "Tag $2-$3 already exists... Skipping..."
+      return
+    fi
+  done
+  docker pull $1:$3
+  docker tag $1:$3 ${DOCKER_USERNAME}/${DOCKER_REPONAME}:$2-$3
+  docker tag $1:$3 quay.io/${DOCKER_USERNAME}/${DOCKER_REPONAME}:$2-$3
+}
+
+# Get all tags
+echo "Get all tags..."
+get_docker_tags https://hub.docker.com/v2/repositories/${DOCKER_USERNAME}/${DOCKER_REPONAME}/tags/?page_size=10000
 
 # Xray DB mongo
 while IFS= read -r version
